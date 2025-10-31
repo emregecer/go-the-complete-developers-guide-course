6fb64c7a4acaf9b6e5d6b1f45aefcfff7cf91adc
Emre Can Gecer <emregecer@gmail.com>
2025-10-31 22:53:45 +0300

    Tests added for newDesk() func

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -9,4 +9,11 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected deck lenght of 16, but got %v", len(d))
 	}
 
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d - 1)]"Four of Clubs" {
+		t.Error
+	}
 }
